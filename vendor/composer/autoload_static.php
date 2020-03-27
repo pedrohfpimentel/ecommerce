9ac9f4e60d8da779ae9d0d2de890cf36622bea1b
pedrohfpimentel <pedrohfpimentel@gmail.com>
2020-03-27 16:26:42 -0300

    template

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0a797436be808adfb8ba7528865dc9d3
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit0a797436be808adfb8ba7528865dc9d3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit0a797436be808adfb8ba7528865dc9d3::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0a797436be808adfb8ba7528865dc9d3::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0a797436be808adfb8ba7528865dc9d3::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit0a797436be808adfb8ba7528865dc9d3::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit0a797436be808adfb8ba7528865dc9d3::$classMap;
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace Hcode;
 
@@ -14,54 +14,56 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array(), $tpl_dir = "/views/"){
-
-		$this->defaults["data"]["session"] = $_SESSION;
+	public function __construct($opts = array())
+	{
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
-					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-					"debug"         => false
-				   );
+		    "base_url"      => null,
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+		    "debug"         => false
+		);
 
-	Tpl::configure( $config );
+		Tpl::configure( $config );
 
-	$this->tpl = new Tpl;
+		$this->tpl = new Tpl();
 
-	$this->setData($this->options["data"]);
+		if ($this->options['data']) $this->setData($this->options['data']);
 
-		if ($this->options["header"] === true) $this->tpl->draw("header");
+		if ($this->options['header'] === true) $this->tpl->draw("header", false);
+
+	}
+
+	public function __destruct()
+	{
+
+		if ($this->options['footer'] === true) $this->tpl->draw("footer", false);
 
 	}
 
 	private function setData($data = array())
 	{
 
-		foreach ($data as $key => $value) {
-			$this->tpl->assign($key, $value);
+		foreach($data as $key => $val)
+		{
+
+			$this->tpl->assign($key, $val);
+
 		}
 
 	}
 
-	public function setTpl($name, $data = array(), $returnHTML = false)
+	public function setTpl($tplname, $data = array(), $returnHTML = false)
 	{
 
 		$this->setData($data);
 
-		$this->tpl->draw($name, $returnHTML);
-		
-	}
-
-	public function __destruct(){
-
-		if ($this->options["footer"] === true) $this->tpl->draw("footer");
+		return $this->tpl->draw($tplname, $returnHTML);
 
 	}
-
-
 
 }
 
-?>
+ ?>
